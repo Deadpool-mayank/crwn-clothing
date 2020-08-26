af34edbd328fd0e6af68469ada4efa0892db6028
DeadPool's DownFall <joshimayank943@gmail.com>
2020-08-26 19:15:14 +0530

    updating stripe integration and updating our favicon and title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -8,6 +8,8 @@ import {selectCartItems,selectCartTotal} from '../../redux/cart/cart.selectors';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
+
 const CheckoutPage = ({cartItems,total}) => (
     <div className='checkout-page'>
         <div className='checkout-header'>
@@ -31,15 +33,19 @@ const CheckoutPage = ({cartItems,total}) => (
                 <span>Remove</span>
             </div>
         </div>
-        {
-            cartItems.map(cartItem =>
-               <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
-            )
-        }
-
+        {cartItems.map(cartItem =>
+         <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
+        )}
         <div className='total'>
             <span>TOTAL: ${total}</span>
         </div>
+        <div className='test-warning'>
+            *Please use the following test credit card for payments*
+            <br/>
+            4242 4242 4242 4242 - Exp: 01/30 - CVV: 123
+        </div>
+        <br/>
+        <StripeCheckoutButton price={total} />
     </div>
 );
 

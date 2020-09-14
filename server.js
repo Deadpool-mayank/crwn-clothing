c55760c3281f76d78ef7a05761c2f1b7a87a187e
DeadPool's DownFall <joshimayank943@gmail.com>
2020-09-14 16:51:36 +0530

    ADDING PWA TO OUR APP

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
-/*const compression = require('compression');*/
+const compression = require('compression');
 const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV != 'production') require('dotenv').config();
@@ -14,22 +14,21 @@ const port = process.env.PORT || 5000;
 
 app.use(compression());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(enforce.HTTPS({trustProtoHeader: true}));
 app.use(cors());
- 
-if (process.env.NODE_ENV === 'production') {
-  /*app.use(compression);*/
-  app.use(enforce.HTTPS({ trustProtoHeader: true }));
-  app.use(express.static(path.join(__dirname, 'client/build')));
- 
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    app.get('*', function(req, res){
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+    });
 }
- 
-app.listen(port, error => {
-  if (error) throw error;
-  console.log('Server is running on port ' + port);
+
+app.listen(port,error => {
+    if(error) throw error;
+    console.log('Server is running on the port '+port);
 });
 
 app.get('/service-worker.js', (req, res) => {

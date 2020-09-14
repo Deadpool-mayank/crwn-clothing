08d4786a7c7d50523ada0050c7c1d7dfb424697d
DeadPool's DownFall <joshimayank943@gmail.com>
2020-09-14 15:59:49 +0530

    ADDING PWA TO OUR APP

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 
+import * as serviceWorker from './serviceWorker';
+
 import './index.css';
 import App from './App';
 
@@ -24,3 +26,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+serviceWorker.register();
